576d7977885dea579d3c483ff80625a64a5a4676
Justin Thomas <jthomas0209@gmail.com>
2016-03-24 10:30:23 -0400

    Done

--- tests/MainController.spec.js
+++ tests/MainController.spec.js
@@ -1,5 +1,23 @@
-describe('example test', function() {
-    it('should be true', function() {
-        expect('foo').toBe('foo');
+describe('MainController', function() {
+    
+    beforeEach(module('app'));
+    
+    var $controller;
+
+    beforeEach(inject(function(_$controller_){
+      $controller = _$controller_;
+    }));
+
+    it('should have Steve Jobs name', function(){
+      var $scope = {};
+      var controller = $controller('MainController', {$scope:$scope});
+      expect(controller.name).toEqual('Steve Jobs');
+    });
+
+    it('should add two numbers', function() {
+      expect(addNumbers(1,3)).toBe(4);
+      expect(addNumbers(92,35)).toBe(127);
+      expect(addNumbers('92',35)).toBe(127);
+      expect(addNumbers('92', '35')).toBe(127);
     });
 });
